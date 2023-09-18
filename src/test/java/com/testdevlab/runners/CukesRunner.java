8de8aed71ad3e090b7e4f0a192688007ed71666b
Albert Shima <albertshima@gmail.com>
2023-09-18 14:14:36 +0200

    3-Adding Runner class and FailedTestRunner class

---
+++ src/test/java/com/testdevlab/runners/CukesRunner.java
@@ -0,0 +1,22 @@
+package com.testdevlab.runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {
+                "html:target/cucumber-reports.html",
+                "rerun:target/rerun.txt",
+                "me.jvt.cucumber.report.PrettyReports:target/cucumber"
+        },
+        features = "src/test/resources/features",
+        glue = "com.testdevlab.step_definitions",
+        dryRun = false,
+        tags = "@Test1",
+        publish = true
+)
+
+public class CukesRunner {
+}
