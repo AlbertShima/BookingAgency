c84b2b771f0008b4ffbaaf2b99178d88d575dd9f
Albert Shima <albertshima@gmail.com>
2023-09-19 00:23:43 +0200

    7-Update Selenium

--- src/test/java/com/testdevlab/pages/Account_Creation_Page.java
+++ src/test/java/com/testdevlab/pages/Account_Creation_Page.java
@@ -34,9 +34,27 @@ public class Account_Creation_Page {
     @FindBy(xpath = "//button[@type=\"submit\"]")
     public WebElement createAccount;
 
+    @FindBy(xpath = "//span[text()=\"Sign in with a verification link\"]/..")
+    public WebElement signInWithAVerificationLink;
 
+    @FindBy(xpath = "//button[@data-testid=\"header-language-picker-trigger\"]")
+    public WebElement backToSignIn;
 
-    @FindBy(xpath = "//p[text()=\"Press and hold\"]")
-    public WebElement pressAndHoldForConfirmation;
+    @FindBy(xpath = "//input[@type=\"email\"]")
+    public WebElement emailOnGmail;
 
+    @FindBy(xpath = "//input[@type=\"password\"]")
+    public WebElement passwordOnGmail;
+
+    @FindBy(xpath = "//*[text()=\"Next\"]")
+    public WebElement nextOnGmail;
+
+    @FindBy(xpath = "(//*[text()=\"Your verification link\"])[2]")
+    public WebElement verificationLinkOnGmail;
+
+    @FindBy(xpath = "")
+    public WebElement verifyMeOnGmail;
+
+    @FindBy(xpath = "//*[@data-tooltip=\"Show trimmed content\"]")
+    public WebElement threeDotsOnGmail;
 }
--- src/test/java/com/testdevlab/step_definitions/Account_Creation_Page_step_definition.java
+++ src/test/java/com/testdevlab/step_definitions/Account_Creation_Page_step_definition.java
@@ -9,13 +9,18 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import org.junit.Assert;
 
+import java.time.Duration;
+
 public class Account_Creation_Page_step_definition {
     Account_Creation_Page account_creation_page = new Account_Creation_Page();
+    Duration timeout = Duration.ofSeconds(5);
 
     @Given("I am in a Sign Up page")
     public void I_am_in_a_Sign_Up_page() {
         Driver.getDriver().get(ConfigurationReader.getProperty("url"));
-        if (BrowserUtils.isElementVisible(account_creation_page.cancelButton, 5)) {
+        if (BrowserUtils.isElementVisible(account_creation_page.cancelButton, timeout)) {
+            account_creation_page.cancelButton.click();
+        } else if (BrowserUtils.isElementVisible(account_creation_page.cancelButton, timeout)) {
             account_creation_page.cancelButton.click();
         }
         account_creation_page.register.click();
@@ -32,6 +37,9 @@ public class Account_Creation_Page_step_definition {
             case "create_account":
                 account_creation_page.createAccount.click();
                 break;
+            case "signin_with_verification_link":
+                account_creation_page.signInWithAVerificationLink.click();
+                break;
             default:
                 throw new IllegalArgumentException("Unknown element: " + webElement);
         }
@@ -44,7 +52,7 @@ public class Account_Creation_Page_step_definition {
 
     @Then("I enter valid password")
     public void I_enter_valid_password() {
-        if (BrowserUtils.isElementVisible(account_creation_page.password, 1)) {
+        if (BrowserUtils.isElementVisible(account_creation_page.password, timeout)) {
             account_creation_page.password.sendKeys(ConfigurationReader.getProperty("password"));
         } else {
             account_creation_page.newPassword.sendKeys(ConfigurationReader.getProperty("password"));
@@ -55,7 +63,19 @@ public class Account_Creation_Page_step_definition {
 
     @And("main page is opened")
     public void mainPageIsOpened() {
-        Assert.assertEquals("Booking.com",Driver.getDriver().getTitle());
+
+        Driver.getDriver().get(ConfigurationReader.getProperty("gmail"));
+        account_creation_page.emailOnGmail.sendKeys(ConfigurationReader.getProperty("email"));
+        account_creation_page.nextOnGmail.click();
+        BrowserUtils.isElementVisible(account_creation_page.passwordOnGmail, timeout);
+        account_creation_page.passwordOnGmail.sendKeys(ConfigurationReader.getProperty("password"));
+        account_creation_page.nextOnGmail.click();
+        BrowserUtils.isElementVisible(account_creation_page.verificationLinkOnGmail, timeout);
+        account_creation_page.verificationLinkOnGmail.click();
+        BrowserUtils.scrollToElement(Driver.getDriver(), account_creation_page.verifyMeOnGmail);
+        account_creation_page.verifyMeOnGmail.click();
+
+
 
     }
 
@@ -72,5 +92,10 @@ public class Account_Creation_Page_step_definition {
 
     @Then("{string} page is opened And correct value is prefilled in email verification placeholder")
     public void pageIsOpenedAndCorrectValueIsPrefilledInEmailVerificationPlaceholder(String arg0) {
+    }
+
+    @Given("I am sing in already with email {string}")
+    public void iAmSingInAlreadyWithEmail(String email) {
+        Driver.getDriver().get(ConfigurationReader.getProperty("logInUrl"));
     }
 }
