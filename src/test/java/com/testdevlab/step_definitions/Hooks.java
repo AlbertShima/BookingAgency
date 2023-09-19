103963daef5a89545e56b6b9a013f7bb24783a54
Albert Shima <albertshima@gmail.com>
2023-09-19 10:32:23 +0200

    9-Adding Hooks class

---
+++ src/test/java/com/testdevlab/step_definitions/Hooks.java
@@ -0,0 +1,24 @@
+package com.testdevlab.step_definitions;
+/*
+In the class we will be able to pass pre- & post- conditions to
+each scenario and each step
+*/
+
+import com.testdevlab.utilities.Driver;
+import io.cucumber.java.After;
+import io.cucumber.java.Scenario;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
+public class Hooks {
+    //import from io.cucumber.java not from unit
+    @After
+    public void tearDown(Scenario scenario) {
+        if (scenario.isFailed()) {
+            byte[] screenShot = ((TakesScreenshot) Driver.getDriver()).getScreenshotAs(OutputType.BYTES);
+            scenario.attach(screenShot, "image/png", scenario.getName());
+        }
+        //Closing the driver after each season is done
+        //Driver.closeDriver();
+    }
+}
